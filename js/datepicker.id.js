1118b5a6a53559f52bdd9d16c67881d0dffde69f
Aditya Wiguna <adityawiguna0@gmail.com>
2020-01-02 10:16:31 +0800

    init commit tugas

---
+++ js/datepicker.id.js
@@ -0,0 +1,15 @@
+;
+(function ($) {
+    $.fn.datepicker.language['en'] = {
+        days: ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'],
+        daysShort: ['Min', 'Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab'],
+        daysMin: ['Mn', 'Sn', 'Sl', 'Rb', 'Km', 'Jm', 'Sa'],
+        months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        today: 'Hari Ini',
+        clear: 'Clear',
+        dateFormat: 'mm/dd/yyyy',
+        timeFormat: 'hh:ii aa',
+        firstDay: 0
+    };
+})(jQuery);
